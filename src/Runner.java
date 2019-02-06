bc43aa7c560423c9632b924ce01f53e19a10b6ba
Braelyn Wekwerth <BraelynWekwerth@students.berkeley.net>
2019-02-06 14:26:13 -0800

    Changed it

--- src/Runner.java
+++ src/Runner.java
@@ -2,10 +2,10 @@ public class Runner {
 
     //Warm-up 1
     public static boolean icyHot(int temp1, int temp2) {
-        if (temp1 <0 && temp2 > 100){
+        if (temp1 < 0 && temp2 > 100) {
             return true;
         }
-        if(temp2<0 && temp1>100){
+        if (temp2 < 0 && temp1 > 100) {
             return true;
         }
         return false;
@@ -13,13 +13,13 @@ public class Runner {
 
     //Warm-up 1
     public static boolean hasTeen(int a, int b, int c) {
-        if (a>=13 && a<=19){
+        if (a >= 13 && a <= 19) {
             return true;
         }
-        if (b>=13 && b<=19){
+        if (b >= 13 && b <= 19) {
             return true;
         }
-        if (c>=13 && c<=19){
+        if (c >= 13 && c <= 19) {
             return true;
         }
         return false;
@@ -28,11 +28,11 @@ public class Runner {
     //Warm-up 2
     public static String stringYak(String str) {
         String y = "";
-        for (int i=0;i<str.length(); i++){
-            if(i+2<str.length() && str.substring(i, i+1).equals("y") && str.substring(i+2, i+3).equals("k")){
-                i=i+2;
-            }else{
-                y += str.substring(i, i+1);
+        for (int i = 0; i < str.length(); i++) {
+            if (i + 2 < str.length() && str.substring(i, i + 1).equals("y") && str.substring(i + 2, i + 3).equals("k")) {
+                i = i + 2;
+            } else {
+                y += str.substring(i, i + 1);
             }
         }
         return y;
@@ -41,13 +41,13 @@ public class Runner {
     //Logic-1: fizzString
     public String fizzString(String str) {
         String b = "";
-        if (str.startsWith("f")){
-            b+="Fizz";
+        if (str.startsWith("f")) {
+            b += "Fizz";
         }
-        if (str.endsWith("b")){
-            b+="Buzz";
+        if (str.endsWith("b")) {
+            b += "Buzz";
         }
-        if (!str.startsWith("f") && !str.endsWith("b")){
+        if (!str.startsWith("f") && !str.endsWith("b")) {
             return str;
         }
         return b;
@@ -55,9 +55,9 @@ public class Runner {
 
     //String 1: hasBad
     public boolean hasBad(String str) {
-        if ((str.indexOf("b")==0 || str.indexOf("b")==1) && (str.indexOf("a")==1 || str.indexOf("a")==2) && (str.indexOf("d")==2 || str.indexOf("d")==3)){
+        if ((str.indexOf("b") == 0 || str.indexOf("b") == 1) && (str.indexOf("a") == 1 || str.indexOf("a") == 2) && (str.indexOf("d") == 2 || str.indexOf("d") == 3)) {
             return true;
-        }else{
+        } else {
             return false;
         }
     }
@@ -65,8 +65,8 @@ public class Runner {
     //String 2: countHi
     public int countHi(String str) {
         int count = 0;
-        for (int i=0; i<str.length(); i++){
-            if (i+1<str.length() && str.substring(i,i+1).equals("h") && str.substring(i+1,i+2).equals("i")){
+        for (int i = 0; i < str.length(); i++) {
+            if (i + 1 < str.length() && str.substring(i, i + 1).equals("h") && str.substring(i + 1, i + 2).equals("i")) {
                 count += 1;
             }
         }
@@ -78,14 +78,13 @@ public class Runner {
         int leng = str.length();
         boolean happy = true;
 
-        for (int i=0; i<leng; i++){
-            if (str.charAt(i) == 'g'){
-                if (i > 0 && str.charAt(i-1) == 'g'){
+        for (int i = 0; i < leng; i++) {
+            if (str.charAt(i) == 'g') {
+                if (i > 0 && str.charAt(i - 1) == 'g') {
                     happy = true;
-                }else
-                if (i < (leng-1) && str.charAt(i+1) == 'g'){
+                } else if (i < (leng - 1) && str.charAt(i + 1) == 'g') {
                     happy = true;
-                }else{
+                } else {
                     happy = false;
                 }
 
@@ -96,50 +95,50 @@ public class Runner {
 
 
     //Array 1: double23
-public boolean double23(int[] nums) {
-    if (nums.length != 1 && nums.length != 0){
-        return ((nums[0] == 2 && nums[1] ==2) || (nums [0] == 3 && nums[1] ==3));
-    }else{
-        return false;
+    public boolean double23(int[] nums) {
+        if (nums.length != 1 && nums.length != 0) {
+            return ((nums[0] == 2 && nums[1] == 2) || (nums[0] == 3 && nums[1] == 3));
+        } else {
+            return false;
+        }
     }
-}
-
-
 
 
     //Array 2: sum13
-public int sum13(int[] nums) {
-    int sum = 0;
-    for (int i = 0; i < nums.length; i++) {
-        if (nums[i] != 13) {
-            sum += nums[i];
-        } else {
-            if (nums[i] == 13 && i < (nums.length - 1)) {
-                nums[i] = 0;
-                nums[i + 1] = 0;
+    public int sum13(int[] nums) {
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 13) {
+                sum += nums[i];
+            } else {
+                if (nums[i] == 13 && i < (nums.length - 1)) {
+                    nums[i] = 0;
+                    nums[i + 1] = 0;
+                }
             }
         }
-    }
-    return sum;
+        return sum;
 
-}
+    }
 
 
     //Array3: canBalance
-public boolean canBalance(int[] nums) {
-    for (int i=0; i<nums.length; i++){
-        int sum = 0;
-        for (int j=0; j<i; j++){
-            sum += nums[j];
+    public boolean canBalance(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            int sum = 0;
+            for (int j = 0; j < i; j++) {
+                sum += nums[j];
+            }
+            for (int p = i; p < nums.length; p++) {
+                sum -= nums[p];
+            }
+            if (sum == 0) {
+                return true;
+            }
         }
-        for (int p=i; p<nums.length; p++){
-            sum -= nums[p];
-        }
-        if (sum ==0){
-            return true;
-        }
+        return false;
     }
-    return false;
+
 }
 
 
